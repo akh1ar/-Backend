b39f8e7ad1ec7a8787e72f00c05aa8ae2e6905b4
akh1ar <shkhan1.jmi@gmail.com>
2025-03-09 22:36:19 +0530

    Routing

--- Topics/Expressjs/index.js
+++ Topics/Expressjs/index.js
@@ -13,23 +13,48 @@ const express = require("express");
 const app = express();
 // console.dir(app);
 /*Port are the logical endpoints of a network connection that is used to exchange information between a web server and a web client. */
-let port = 8080;
+let port = 3000;
 //listen is accpet two parameter
 app.listen(port,()=>{
     console.log(`app is listening on port ${port}`);
 });
 
 /*Handling requests*/
-app.use((req,res) =>{
-    // console.log(req);
-    console.log("request receveid");
-    //send request with content-type of text
-    res.send("this is basics respose");
-    // send request with content-type of josn
-    // res.send({
-    //     name:"apple",
-    //     color:"red",
-    // });
-})
+// app.use((req,res) =>{
+//     /*Sending a Response
+//     res.send is used to send the request from our server*/
+//     console.log("request recevied");
+//     //send request with content-type of text
+//     res.send("this is basics respose");
+// });
 
-/*Sending a Response*/
+/**Routing
+ it is process of selecting a path for traffic in a network or between or across multiple networks.
+*/
+app.get("/",(req,res)=>{
+    res.send("You connected root path");
+});
+app.get("/obj",(req,res)=>{
+    // send request with content-type of josn
+    res.send({
+        name:"apple",
+        color:"red",
+    });
+});
+
+app.get("/html",(req,res)=>{
+    //send html contntent
+    let code  = `<h1>Shahnawaz</h1><ul><li>code</li><li>sleep</li></ul>`
+    res.send(code);
+});
+
+//when this valid routes is not define when enter the worng path give error 400 page not found
+app.get("*",(req,res)=>{
+    res.send("this path is not vaild");
+});
+
+app.post("/",(req,res)=>{
+    res.send("you conncted root for post method");
+});
+
+//read about routing get and post method
